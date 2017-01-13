723d1b91ba0fa8f150cbc1544697ee25d4d8f886
Monroe Mann <findingsummer@aol.com>
2017-01-13 23:28:52 +0000

    Created add place form with simpleform gem

--- Gemfile
+++ Gemfile
@@ -33,6 +33,12 @@ gem 'rails_12factor', group: :production
 gem 'bootstrap-sass','~>3.3.5.1'
 gem 'sprockets', '2.11.0'
 
+# Add will_paginate gem
+gem 'will_paginate', '~> 3.0' 
+
+# Add simple_form gem
+gem 'simple_form'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,14 @@
 class PlacesController < ApplicationController
   
   def index
-    @places = Place.all
+    @places = Place.released.paginate(page: params[:page], per_page: 3)
+
+    #ALTERNATE METHOD.  DOES SAME THING:
+    # @places = Place.paginate(:page => params[:page], :per_page => 3)
+  end
+
+  def new
+    @place=Place.new
   end
 
 end
