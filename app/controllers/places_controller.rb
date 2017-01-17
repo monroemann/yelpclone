c44a662df9d068d8e3cc5a2f74b824e4ae2cebcd
Monroe Mann <findingsummer@aol.com>
2017-01-17 23:51:35 +0000

    Added authentication for edit, update, and delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,8 +13,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-      current_user.places.create(place_params)
-      redirect_to root_path
+      @place = current_user.places.create(place_params)
+        if@place.valid?
+          redirect_to root_path
+        else
+          render :new, status: :unprocessable_entity
+        end
   end
 
   def show
@@ -38,7 +42,12 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+        if@place.valid?
+          redirect_to root_path
+        else
+          render :new, status: :unprocessable_entity
+        end
+
   end
 
   def destroy
@@ -47,7 +56,7 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render text: 'You are not authorized to visit this page.', status: :forbidden  
     end
-    
+
     @place.destroy
     redirect_to root_path
   end
