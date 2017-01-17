c2fc6f7635211498d8c4b1b4a29e05fe9aceb65a
Monroe Mann <findingsummer@aol.com>
2017-01-17 18:21:30 +0000

    Added delete post functionality; only user creator can delete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create, :edit, :update]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
   
   def index
     @places = Place.order("created_at ASC").all.paginate(page: params[:page], per_page: 3)
@@ -43,6 +43,11 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'You are not authorized to visit this page.', status: :forbidden  
+    end
+    
     @place.destroy
     redirect_to root_path
   end
