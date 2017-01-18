5178cdfde9c473beda08fd9f597d505f0e57a4dd
Monroe Mann <findingsummer@aol.com>
2017-01-18 00:59:54 +0000

    Add validation to name field minimum, address required, and description required

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ActiveRecord::Base
 
 belongs_to :user
-validates :name, presence: true
+validates :name, :address, :description, presence: true
+validates :name, length: { minimum: 3, 
+  too_short: "3 characters is the minimum allowed." }
 
 end
