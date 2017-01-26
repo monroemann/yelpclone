1f9c3eabc0a67416380bf149b18fba18558b2787
Monroe Mann <findingsummer@aol.com>
2017-01-26 01:38:40 +0000

    made some changes to photos_controller and show.html

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,13 +2,10 @@ class PhotosController < ApplicationController
 
   def create
 
-   @photo = current_user.photos.create(photo_params)
-        if@photo.valid?
-          redirect_to root_path
-        else
-          render :new, status: :unprocessable_entity
-        end
-
+    place = Place.find(params[:id])
+    @photo = place.photos.create(photo_params)
+    redirect_to place_path(place)
+        
   end
 
 
